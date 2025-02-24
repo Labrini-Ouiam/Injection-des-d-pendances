d94272b22d7fa7e1fc8a7aece22f590a692f538c
LABRINI <labriniouiam@gamil.com>
2025-02-24 10:59:08 +0100

    Ajout de l'interface IMetier avec la méthode calcul et implémentation avec un couplage faible

---
+++ src/metier/MetierImpl.java
@@ -0,0 +1,22 @@
+package metier;
+
+import dao.DaoImpl;
+import dao.IDao;
+
+public class MetierImpl implements IMetier{
+    //Coublage faible
+    private IDao dao;
+
+    @Override
+    public double calcul() {
+        double t= dao.getData();
+        double res =t*23;
+        return res;
+    }
+
+    //pour injecter dans la variable dao un objet
+    //d'une class qui implement l'interface Idao
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
